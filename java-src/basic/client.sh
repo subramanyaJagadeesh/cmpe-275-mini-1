1cf5295a045b24cba2e5dc1934ea82a26f5002d6
ArmaghanAb <armaghanabtahi@gmail.com>
2024-02-20 00:57:59 -0800

    java-src

--- java-src/basic/client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# run the client. See server.sh for comments.
-
-#JAVA_HOME=/where/am/i
-#${JAVA_HOME}/java -cp .:./classes:lib/*.jar gash.app.BasicClientApp 
-
-java -cp .:./classes:lib/*.jar gash.app.ClientApp 
-
